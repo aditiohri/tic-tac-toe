35f0b8f00f744005f1a3f91ce2ec80d6fb6edbf9
Aditi Ohri <didiohri@gmail.com>
2019-10-25 15:05:41 -0500

    update js variables

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 /*----- constants -----*/
+const winCombo = {
+    
+}
+
+
 /*----- app's state (variables) -----*/
+
+let message = document.getElementById('message');
+let turn = document.getElementById('turn');
+let gameBoard = document.getElementById('tictactoe')
+
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+// function init();
 
 function render() {
 	renderHands();
