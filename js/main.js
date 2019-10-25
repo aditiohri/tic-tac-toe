9089baed7959fc4c4ada2ace2aeee034577837c4
Aditi Ohri <didiohri@gmail.com>
2019-10-25 13:15:39 -0500

    update html and css gambeboard

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-alert('you can do')
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+function render() {
+	renderHands();
+	renderControls();
+	if (winner) {
+		renderWinnerMessage();
+	} else {
+		renderTurnMessage();
+	}
+}
