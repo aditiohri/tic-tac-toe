c1930c59d01758fe2a77c8d07f20940cbecbbe71
Aditi Ohri <didiohri@gmail.com>
2019-10-25 10:29:21 -0500

    create file structure

---
+++ js/main.js
@@ -0,0 +1,6 @@
+alert('you can do')
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
