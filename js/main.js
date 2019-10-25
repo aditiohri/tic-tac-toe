1a241c83020e1d4344498e3b0decd12fb4188f42
Aditi Ohri <didiohri@gmail.com>
2019-10-25 16:16:12 -0500

    update js state

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
 /*----- constants -----*/
+const max_number_plays = 9;
 const winCombo = {
+   
     
 }
-
+const playerX = 1;
+const playerO = -1;
 
 /*----- app's state (variables) -----*/
 
-let message = document.getElementById('message');
-let turn = document.getElementById('turn');
-let gameBoard = document.getElementById('tictactoe')
 
 
 /*----- cached element references -----*/
+let message = document.getElementById('message');
+let turn = document.getElementById('turn');
+let td = document.querySelectorAll('td');
+let gameBoard = document.getElementById('tictactoe');
+console.log(td)
+// console.log(turn)
+// console.log(gameBoard)
+
 /*----- event listeners -----*/
+document.getElementsByTagName('tictactoe')
+    .addEventListener('click', handleTDClick)
+
 /*----- functions -----*/
 
 // function init();
+
+function handleTDClick(evt) {
+    console.log(evt.target.textContent)
+}
 
 function render() {
 	renderHands();
