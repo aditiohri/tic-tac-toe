18bce849b1ece19dfafab91b11de20133e5251bf
Aditi Ohri <didiohri@gmail.com>
2019-10-25 19:51:53 -0500

    update game logic and UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*----- constants -----*/
 const max_number_plays = 9;
+const EMPTY = " ";
 const winCombo = [  
     ["one", "two", "three"],
     ["four", "five", "six"],
@@ -18,8 +19,7 @@ const playerX = "X";
 
 /*----- app's state (variables) -----*/
 
-let playCount = [];
-let playCombo = [];
+let playLetter = []; let playCount = []; let playCombo = [];
 
 
 /*----- cached element references -----*/
@@ -28,13 +28,17 @@ let turn = document.getElementById('turn');
 let td = document.querySelectorAll('td');
 let gameBoard = document.getElementById('tictactoe');
 let turnEl = document.getElementById('turn');
-let button = document.getElementsByTagName('button');
 
 
 /*----- event listeners -----*/
-document.getElementById('tictactoe')
-    .addEventListener('click', handleTDClick)
 
+document.getElementById('tictactoe')
+    .addEventListener('click', handleTDClick);
+
+document.getElementById('replay')
+    .addEventListener('click', rePlay);
+
+    
 /*----- functions -----*/
 // init();
 
@@ -46,26 +50,26 @@ function handleTDClick(evt) {
     // playCombo.push(playLetter.id);
     // do the above for each player? 
     // make it impossible for a player to change the value of the previous play on a td
-
+    
 }
 
-// function renderHands(); {
-
-// }
-// function renderControls();{
-
-// }
-
-// function renderWinnerMessage() {
-
-// }
 
 function renderTurnMessage() {
     return (playCount.length % 2 ? turnEl.innerHTML = `Your turn: <br> Player O` : turnEl.innerHTML = `Your turn: <br> Player X`)
 }
-
+        
 // create a message for winner
+        
 // replay button clears the gameboard
+function rePlay() {
+    playCount = []; 
+    td.forEach(function(box) {
+        box.textContent = EMPTY;
+    } );
+    turnEl.innerHTML = `Your turn: <br> Player X`;
+}
+
+
 // create separate arrays for O and X position plays
 // compare with winning count
 // cannot strictly compare array
