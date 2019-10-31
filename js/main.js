9eaac60acee10d3ae46a4db8a08b014aa1d8b98a
Aditi Ohri <didiohri@gmail.com>
2019-10-31 14:50:52 -0500

    update win logic

--- js/main.js
+++ js/main.js
@@ -12,8 +12,6 @@ const winCombo = [
     ["three", "five", "seven"],
  ];
 
-
-
 const playerO = "O";
 const playerX = "X";
 
@@ -45,6 +43,8 @@ replayButton.addEventListener('click', init);
 
     
 /*----- functions -----*/
+//initialize game variables and board
+init();
 
 // when gameboard event listener is activated
 function handleTDClick(evt) {
@@ -52,25 +52,39 @@ function handleTDClick(evt) {
     playSpace = evt.target;
     playCount.push(playSpace);
     play();
-
 }
 
 // play as X or O
 function play() {
     if (playSpace.textContent === ("X") || (playSpace.textContent === ("O"))) {
         return 
-    } 
-    else if (playCount.length % 2 === 1) {
+    } else if (playCount.length % 2 === 1) {
         playSpace.textContent = playerX;
         playerXScore.push(playSpace.id)
+        checkWin(playerXScore);
     } else if (playCount.length % 2 === 0) {
         playSpace.textContent = playerO;
         playerOScore.push(playSpace.id)
-
-    }
-    renderTurnMessage();
-    return   
+        checkWin(playerOScore);
+    } renderTurnMessage();   
 };
+
+function checkWin(playerScore) {
+if (playCount.length > 2) {
+    for (let idx = 0; idx < winCombo.length; i++) {
+        for (let i = 0; i < winCombo[idx].length; i++)
+            if (playerScore.includes(winCombo[idx][i])) {
+            console.log('ran' +i);
+            winMsg();
+        } else { return }
+    }
+}
+};
+    //look through the winCombo arrays 
+    //check each array to see if playerXScore incldues the items of the arrays
+    //check above for playerOScore too
+    //if matches print win message for winning player
+    // cannot strictly compare array //flatmap //mapfilter //reduce //check arrays lab
 
 
     // alternating message consistent with player
@@ -84,13 +98,20 @@ function renderTurnMessage() {
     } else if (playCount.length === max_number_plays) {
         turnEl.innerHTML = `It's a draw!`
     }
+    return
 } 
-// create a message for winner
-// create game logic
-// create separate arrays for O and X position plays // sort
-// compare with winning count
-// cannot strictly compare array //flatmap //mapfilter //reduce //check arrays lab
-// create loop to compare values 
+
+//displays winning message
+function winMsg() {
+    if (playerOScore.length > playerXScore.length) {
+        turnEl.innerHTML = `Player O is the winner!`
+    }
+    if (playerXScore.length > playerOScore.length) {
+        turnEl.innerHTML = `Player X is the winner!`
+    }
+    return
+}
+
         
 // initialize gameboard
 // replay button clears the gameboard
@@ -104,5 +125,6 @@ function init() {
     td.forEach(function(box) {
         box.textContent = EMPTY;
     } );
+    renderTurnMessage();
 }
 
